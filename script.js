ca2b6d904d8fc80786f6f0d67a0cc24a76fa7252
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-02-23 10:05:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	let inp=document.querySelector("input")
+	let first_name=inp[0].value;
+	let last_name=inp[1].value;
+	let fullname=first_name+" "+last_name;
+	alert(fullname)
 
 }
